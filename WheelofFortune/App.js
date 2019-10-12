c37febdebb92c48e09a0fca15d3beb7963753049
dangolbeeker <scottb305@outlook.com>
2019-10-12 19:12:24 -0400

    centroid arc for better x, y cordinates/Random math value between 200-2200

--- WheelofFortune/App.js
+++ WheelofFortune/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text as RNText, View, Dimensions } from 'react-native';
 import { GestureHandler, Svg } from 'expo'
 import * as d3Shape from 'd3-shape';
 import color from 'randomcolor';
 import {snap} from '@popmotion/popcorn';
 const { PanGestureHandler, State} = GestureHandler;
 const {Path, G, Text, Tsplash} = Svg;
+const { width } = Dimensions.get('screen');
 
 const numberOfSegments = 10;
 const makeWheel = () => {
   const data = Array.from({length: numberOfSegments}).fill(1);
+  const arcs = d3Shape.pie()(data);
+  const colors = color({
+    luminosity: 'dark',
+    count: numberOfSegments
+  })
+
+  return arcs.map((arc, index) => {
+    const instance = d3Shape
+    .arc()
+    .padAngle(0.01)
+    .outerRadius(width / 2)
+    .innerRadius(20);
+
+    return {
+      path: instance(arc),
+      color: colors[index],
+      value: Math.round(Math.random() * 10 + 1) * 200, //[200, 2200]
+      centroid: instance.centroid(arc)
+    }
+    
+  })
 }
 
 export default class App extends Component {
